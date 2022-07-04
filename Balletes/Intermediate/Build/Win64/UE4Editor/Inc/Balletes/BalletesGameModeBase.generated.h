86ebb7c7d2a548ca550c2f290bb3e503af3be0ea
Hugo <hugo.filipereis@hotmail.com>
2022-07-04 16:25:54 +0100

    Project set up

---
+++ Balletes/Intermediate/Build/Win64/UE4Editor/Inc/Balletes/BalletesGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BALLETES_BalletesGameModeBase_generated_h
+#error "BalletesGameModeBase.generated.h already included, missing '#pragma once' in BalletesGameModeBase.h"
+#endif
+#define BALLETES_BalletesGameModeBase_generated_h
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_SPARSE_DATA
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_RPC_WRAPPERS
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABalletesGameModeBase(); \
+	friend struct Z_Construct_UClass_ABalletesGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABalletesGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Balletes"), NO_API) \
+	DECLARE_SERIALIZER(ABalletesGameModeBase)
+
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABalletesGameModeBase(); \
+	friend struct Z_Construct_UClass_ABalletesGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABalletesGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Balletes"), NO_API) \
+	DECLARE_SERIALIZER(ABalletesGameModeBase)
+
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABalletesGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABalletesGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABalletesGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABalletesGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABalletesGameModeBase(ABalletesGameModeBase&&); \
+	NO_API ABalletesGameModeBase(const ABalletesGameModeBase&); \
+public:
+
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABalletesGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABalletesGameModeBase(ABalletesGameModeBase&&); \
+	NO_API ABalletesGameModeBase(const ABalletesGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABalletesGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABalletesGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABalletesGameModeBase)
+
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_12_PROLOG
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_SPARSE_DATA \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_RPC_WRAPPERS \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_INCLASS \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Balletes_Source_Balletes_BalletesGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_SPARSE_DATA \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Balletes_Source_Balletes_BalletesGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BALLETES_API UClass* StaticClass<class ABalletesGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Balletes_Source_Balletes_BalletesGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Balletes/Source/Balletes/BalletesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BalletesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLETES_API ABalletesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
